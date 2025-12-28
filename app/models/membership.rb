cfc152f2838db91ab9248f2ee413978f4fe9cd4d
Aman <amantank187@gmail.com>
2025-12-28 15:51:58 +0000

    feat: :sparkles: Add membership with default role of viewer

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,6 @@
 class Membership < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+    enum :role, { viewer: "viewer", editor: "editor", admin: "admin" }, default: :viewer
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,8 +1,14 @@
 require "rails_helper"
 
-RSpec.describe Memebership, type: :model do
+RSpec.describe Membership, type: :model do
   describe "validations" do
-    it "membership has user, project and role" do
+    it "membership default role of viewer" do
+      creator = User.create(email: "test@example.com")
+      project = Project.create(name: "test project", creator: creator)
+      invitee = User.create(email: "invite@example.com")
+
+      membership = Membership.create(project: project, user: invitee)
+      expect(membership.role).to eq("viewer")
     end
   end
 end
